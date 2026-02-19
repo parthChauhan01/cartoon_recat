f2345a292ea543bac3c00bfac17dcd8532c0ad68
parthChauhan01 <parthmchauhan01@gmail.com>
2026-02-19 14:05:29 +0530

    Fix base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'cartoon_recat', // Replace with your actual repo name
+  base: '/cartoon_recat', // Replace with your actual repo name
 })
